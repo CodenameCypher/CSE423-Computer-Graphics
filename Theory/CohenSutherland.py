089fc8ae4cf847cac472f7d60c97bbdf627de89a
CodenameCypher <adityaroy083@gmail.com>
2021-07-31 15:21:11 +0600

    cohen fix

--- Theory/CohenSutherland.py
+++ Theory/CohenSutherland.py
@@ -26,17 +26,12 @@ def outcode(x, y):
     return code
 
 
-def convertBinaryToDecimal(s):
-    n = int(s)
-    decimalNumber = 0
-    i = 0
-    remainder = None
-    while(n != 0):
-        remainder = n % 10
-        n /= 10
-        decimalNumber += remainder*pow(2, i)
-        i += 1
-    return int(decimalNumber)
+def checkBits(bit1, bit2):
+    for i in range(4):
+        if bit1[i] == bit2[i] and bit1[i] == '1':
+            return True
+
+    return False
 
 
 def cohen(x1, y1, x2, y2):
@@ -44,58 +39,69 @@ def cohen(x1, y1, x2, y2):
     oc2 = outcode(x2, y2)
     m = (y2-y1)/(x2-x1)
     print("m", m)
+    print('--------------------------------------------------------------')
     x_1 = x1
     y_1 = y1
     x_2 = x2
     y_2 = y2
     while(True):
-        dec1 = convertBinaryToDecimal(oc1)
-        dec2 = convertBinaryToDecimal(oc2)
-        print(oc1, ' ', oc2)
+        print('Current Outcode 1:', oc1)
+        print('Current Outcode 2:', oc2)
         if oc1 == oc2 and oc1 == "0000":
-            print(x_1, " ", y_1, " , ", x_2, " ", y_2)
+            print('Final Points: (', x_1, ",", y_1, ") , (", x_2, ",", y_2, ')')
             break
-        elif((dec1 & dec2) != 0):
+        elif(checkBits(oc1, oc2)):
             print("point completely outside")
             break
         else:
-            x = x1, y = y1
+            x = x1
+            y = y1
             if(oc1 != "0000"):
                 if(oc1[0] == '1'):
                     y = y_max
                     x = round(x1 + 1/m*(y_max - y1))
+                    print('Updated top for the first outcode')
                 elif(oc1[1] == '1'):
                     y = y_min
                     x = round(x1 + 1/m*(y_min - y1))
+                    print('Updated bottom for the first outcode')
                 elif(oc1[2] == '1'):
                     x = x_max
                     y = round(y1 + m*(x_max - x1))
+                    print('Updated right for the first outcode')
                 elif(oc1[3] == '1'):
                     x = x_min
                     y = round(y1+m*(x_min-x1))
+                    print('Updated left for the first outcode')
                 oc1 = outcode(x, y)
                 x_1 = x
                 y_1 = y
 
             elif(oc2 != "0000"):
-                x = x1, y = y1
+                x = x1
+                y = y1
                 if(oc2[0] == '1'):
                     y = y_max
                     x = round(x2 + 1/m*(y_max - y2))
+                    print('Updated top for the second outcode')
                 elif(oc2[1] == '1'):
                     y = y_min
                     x = round(x2 + 1/m*(y_min - y2))
+                    print('Updated bottom for the second outcode')
                 elif(oc2[2] == '1'):
                     x = x_max
                     y = round(y2 + m*(x_max - x2))
+                    print('Updated top for the second outcode')
                 elif(oc2[3] == '1'):
                     x = x_min
                     y = round(y2+m*(x_min-x2))
+                    print('Updated top for the second outcode')
                 oc2 = outcode(x, y)
                 x_2 = x
                 y_2 = y
 
-            print(x_1, " ", y_1, " , ", x_2, " ", y_2)
+            print('Updated Points: (', x_1, ",", y_1, ") , (", x_2, ",", y_2, ')')
+            print('--------------------------------------------------------------')
         continue
 
 
